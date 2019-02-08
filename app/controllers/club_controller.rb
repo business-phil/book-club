c31eda6c9dc19e7ac8ead9e673299ac31af576a0
Phil <phil.boothe@gmail.com>
2019-02-08 11:13:57 -0500

    add club#show route

--- app/controllers/club_controller.rb
+++ app/controllers/club_controller.rb
@@ -1,4 +1,9 @@
 class ClubController < ApplicationController
   def index
+    @clubs = Club.all
+  end
+
+  def show
+    @club = Club.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :club, only: [:index]
+  resources :club, only: [:index, :show]
 end
