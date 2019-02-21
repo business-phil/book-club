db1069a7c69492cb647ab00bc786bd4a487f74e9
Phil <phil.boothe@gmail.com>
2019-02-21 13:18:34 -0500

    Add scaffold for members

--- app/models/member.rb
+++ app/models/member.rb
@@ -2,4 +2,5 @@
 
 class Member < ApplicationRecord
   has_and_belongs_to_many :clubs
+  has_secure_password
 end
--- db/migrate/20190208150329_create_members.rb
+++ db/migrate/20190208150329_create_members.rb
@@ -5,9 +5,11 @@ class CreateMembers < ActiveRecord::Migration[5.2]
     create_table :members do |t|
       t.string :name
       t.string :email
+      t.string :password_digest
 
       t.timestamps
     end
+    add_index :members, :email, unique: true
 
     create_table :clubs_members do |t|
       t.belongs_to :club, foreign_key: true
